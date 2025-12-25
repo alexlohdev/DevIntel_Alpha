04cf95c6366bfdec64600222500d8dca12fc4c81
alexlohdev <alex@illimitecreative.com>
2025-12-25 14:43:48 +0800

    Update teduh_scraper_v2.py

--- teduh_scraper_v2.py
+++ teduh_scraper_v2.py
@@ -1,3 +1,8 @@
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import ElementClickInterceptedException
+
 import os
 import re
 import time
